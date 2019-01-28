3d34f3ff2d83cfa65ebeb788f489001735918c73
Zaahir Moolla <moollaza@duckduckgo.com>
2019-01-27 23:32:43 -0500

    Fix mixins

--- src/mixins.js
+++ src/mixins.js
@@ -1,9 +1,27 @@
-export function getSelectedRepos() {
-  return this.$root.$data.repos.filter(function (repo) {
-    return repo.selected;
-  });
-}
+export const selectedRepos = {
+  computed: {
+    numberOfSelectedRepos() {
+      const selected = this.$root.$data.repos.filter(function (repo) {
+        return repo.selected;
+      });
 
-export function hasSelectedRepos() {
-  return !!this.getSelectedRepos();
+      return selected.length;
+    }
+  },
+  methods: {
+    getSelectedRepos() {
+      return this.$root.$data.repos.filter(function (repo) {
+        return repo.selected;
+      });
+    },
+    hasSelectedRepos() {
+      const repos = this.getSelectedRepos();
+      return repos.length > 0;
+    },
+    numSelectedRepos() {
+      if (!this.hasSelectedRepos()) return 0;
+      const selectedRepos = this.getSelectedRepos();
+      return selectedRepos.length;
+    }
+  }
 }
