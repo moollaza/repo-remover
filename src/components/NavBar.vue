0f8d188f1ea4e89fb405c39c61d07d8071b28b68
Zaahir Moolla <moollaza@duckduckgo.com>
2019-02-01 21:14:07 -0500

    Wrap navbar content in container

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,25 +4,27 @@
     type="light"
     variant="light"
   >
-    <b-navbar-toggle target="nav_collapse" />
+    <div class="container">
+      <b-navbar-toggle target="nav_collapse" />
 
-    <b-navbar-brand href="/">
-      Repo Remover
-    </b-navbar-brand>
+      <b-navbar-brand href="/">
+        Repo Remover
+      </b-navbar-brand>
 
-    <b-collapse
-      id="nav_collapse"
-      is-nav
-    >
-      <b-navbar-nav>
-        <b-nav-item href="/">
-          Home
-        </b-nav-item>
-        <b-nav-item href="/about">
-          About
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-collapse
+        id="nav_collapse"
+        is-nav
+      >
+        <b-navbar-nav>
+          <b-nav-item href="/">
+            Home
+          </b-nav-item>
+          <b-nav-item href="/about">
+            About
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
   </b-navbar>
 </template>
 
