ee267c7cadb50b11e4501939f368da351dd58a7f
Zaahir Moolla <moollaza@duckduckgo.com>
2019-02-07 22:43:54 -0500

    Add sticky footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    class="d-flex flex-column"
+  >
     <NavBar v-if="$route.name !== 'home'" />
     <div class="container pt-3 pt-sm-5">
       <router-view />
     </div>
+    <footer class="mt-auto text-center py-4 bg-light">
+      <small>
+        Copyright © 2019 Zaahir Moolla
+      </small>
+    </footer>
   </div>
 </template>
 
@@ -23,6 +31,19 @@ export default {
 
 html {
   font-size: 0.9rem;
+}
+
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+
+#app > .container {
+  flex: 1 0 auto;
+}
+
+footer {
+  flex-shrink: 0;
 }
 
 @include media-breakpoint-up(sm) {
