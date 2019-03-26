a09ef7c80426c0f54457b57599225426bb26a9fe
Zaahir Moolla <moollaza@duckduckgo.com>
2019-03-26 02:25:58 -0400

    Fix about page 🤦🏾‍♂️

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,49 +1,58 @@
 <template>
-  <div class="about">
-    <h1 class="display-4">
-      About
-    </h1>
+  <main>
+    <section>
+      <div class="container">
+        <h1 class="title is-size-1">
+          About
+        </h1>
 
-    <p class="lead">
-      Repo Remover makes it easy to archive and delete your GitHub Repos.
-    </p>
+        <p class="is-size-4">
+          Repo Remover makes it easy to archive and delete your GitHub Repos.
+          The GitHub API is used to safely and securely modify your repos.
+        </p>
 
-    <h3 class="mt-5">
-      Made with:
-    </h3>
-    <ul>
-      <li>
-        <b-link
-          class="text-dark"
-          href="https://vuejs.org/"
-        >
-          Vue.js
-        </b-link>
-      </li>
-      <li>
-        <b-link
-          class="text-dark"
-          href="https://vue-apollo.netlify.com/"
-        >
-          Vue Apollo
-        </b-link>
-      </li>
-      <li>
-        <b-link
-          class="text-dark"
-          href="https://octokit.github.io/rest.js/"
-        >
-          Octokit Rest
-        </b-link>
-      </li>
-      <li>
-        <b-link
-          class="text-dark"
-          href="https://buefy.org/"
-        >
-          Buefy
-        </b-link>
-      </li>
-    </ul>
-  </div>
+        <div class="content">
+          <h3>
+            Repo Remover was made with:
+          </h3>
+          <ul class="text-dark">
+            <li>
+              <a href="https://vuejs.org/">
+                Vue.js
+              </a>
+            </li>
+            <li>
+              <a href="https://vue-apollo.netlify.com/">
+                Vue Apollo
+              </a>
+            </li>
+            <li>
+              <a href="https://octokit.github.io/rest.js/">
+                Octokit Rest
+              </a>
+            </li>
+            <li>
+              <a href="https://buefy.org/">
+                Buefy
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+section {
+  padding: 3rem 1.5rem;
+}
+
+.content {
+  margin-top: 2em;
+
+  ul {
+    font-size: 1.5em;
+  }
+}
+</style>
