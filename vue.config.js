11579aac58b33079fa03f3a8a5daacdae8d1a51e
Zaahir Moolla <moollaza@duckduckgo.com>
2019-03-26 00:27:01 -0400

    Cleanup vue config

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,11 @@
 // const glob = require('glob-all');
-const path = require('path');
 
 module.exports = {
   lintOnSave: false,
   productionSourceMap: false,
   css: {
-    sourceMap: true,
-    loaderOptions: {
-      sass: {
-        // data: `@import "@/scss/_base.scss";`,
-        // options: {
-        //   includePaths: ["node_modules"]
-        // }
-      }
-    }
+    sourceMap: true
   },
-  // pluginOptions: {
-  //   'style-resources-loader': {
-  //     preProcessor: 'scss',
-  //     patterns: [
-  //       path.resolve(__dirname, 'src/scss/_base.scss'),
-  //     ]
-  //   }
-  // },
   configureWebpack: {
     plugins: [
     ]
