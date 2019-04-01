b823824d694120294945760b33a150f483f65997
Zaahir Moolla <moollaza@duckduckgo.com>
2019-04-01 00:38:34 -0400

    Fix 404 page layout

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,29 @@
 <template>
   <main>
-    <h1 class="title is-1">
-      404 - Page Not Found
-    </h1>
-    <router-link
-      to="/"
-      class="button is-link"
-    >
-      Go Home
-    </router-link>
+    <section class="padded">
+      <div class="container">
+        <h1 class="title is-1">
+          404 - Page Not Found
+        </h1>
+        <h2 class="subtitle">
+          We can't seem to find the page you're looking for.
+        </h2>
+
+        <p>
+          <router-link
+            to="/"
+            class="button is-link is-medium"
+            type="submit"
+          >
+            <b-icon
+              icon="chevron-circle-left"
+              size="is-small"
+            />
+            <span>Go Home</span>
+          </router-link>
+        </p>
+      </div>
+    </section>
   </main>
 </template>
 
